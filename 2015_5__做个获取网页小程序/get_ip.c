04dc578a1062081eb74e50de5395fa8cf1ffab53
root <root@localhost.localdomain>
2015-05-27 03:44:46 +0800

    今天提交一个把网址转换成IP的小程序

---
+++ 2015_5__做个获取网页小程序/get_ip.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+char *get_ip(char *host);
+
+int main(int argc, char ** argv)
+{
+	char *host;
+	host = argv[1];
+	printf("%s",get_ip(host));	
+return 0;
+}
+
+char *get_ip(char *host)
+{
+  struct hostent *hent;
+  int iplen = 15; //XXX.XXX.XXX.XXX
+  char *ip = (char *)malloc(iplen+1);
+  memset(ip, 0, iplen+1);
+  if((hent = gethostbyname(host)) == NULL)
+  {
+    herror("Can't get IP");
+    exit(1);
+  }
+  if(inet_ntop(AF_INET, (void *)hent->h_addr_list[0], ip, iplen) == NULL)
+  {
+    perror("Can't resolve host");
+    exit(1);
+  }
+  return ip;
+}
